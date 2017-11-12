72c8c14492784f314ffbb62e15404ac94ac1ad84
Daniel Magevski <daniel_magevski@hotmail.com>
2017-11-12 19:13:22 -0200

    root privileges

--- dhcpServer_scapy.py
+++ dhcpServer_scapy.py
@@ -1,6 +1,12 @@
-#This program need to the scapy installed 
+#!/usr/bin/python
 from scapy.all import DHCP_am
 from scapy.base_classes import Net
+import os
+ 
+if os.geteuid() != 0:
+	exit("You need to root privileges")
+
+print("\nPress Ctrl+C to stop\n")
 
 dhcp_server = DHCP_am(iface='eth1', domain='danielbr.me',
                       pool=Net('172.16.0.0/24'),
